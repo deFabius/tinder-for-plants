b2b30865c6989177cc5316ca70650ed4df1e29d9
Fabio Valle <fabio.valle@gmail.com>
2020-09-13 23:36:24 +0100

    Initial logic for login and sign up

--- src/components/screens/LoginScreen/index.js
+++ src/components/screens/LoginScreen/index.js
@@ -1,8 +1,26 @@
-import * as React from 'react';
-import {SafeAreaView, ScrollView, View, Text, Button} from 'react-native';
+import React, { useState } from 'react';
+import auth from '@react-native-firebase/auth';
+
+import { SafeAreaView, ScrollView, View, Text, Button, TextInput } from 'react-native';
 import styles from '../../shared/styles';
 
-function LoginScreen({navigation}) {
+function LoginScreen({ navigation }) {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const Login = e => {
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        navigation.navigate('Main');
+      })
+      .catch(error => {
+        alert('Log in failed');
+
+        console.error(error);
+      });
+  }
+
   return (
     <>
       <SafeAreaView>
@@ -12,6 +30,22 @@ function LoginScreen({navigation}) {
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Log in</Text>
+              <TextInput
+                value={email}
+                onChangeText={setEmail}
+                keyboardType="email-address"
+                style={styles.textInput}
+                placeholder="Enter your email" />
+              <TextInput
+                value={password}
+                onChangeText={setPassword}
+                style={styles.textInput}
+                secureTextEntry={true}
+                placeholder="Enter your password" />
+              <Button
+                title="Log in"
+                onPress={Login} />
+
               <Button
                 title="Sign up"
                 onPress={() => navigation.navigate('Sign up')}
--- src/components/screens/SignUpScreen/index.js
+++ src/components/screens/SignUpScreen/index.js
@@ -1,9 +1,33 @@
-import * as React from 'react';
+import React, { useState } from 'react';
+import auth from '@react-native-firebase/auth';
+
 import { SafeAreaView, ScrollView, View, Text, Button, TextInput, TouchableHighlight } from 'react-native';
 import styles from '../../shared/styles';
 import SignUpWithGoogleIcon from '../../shared/icons/btn_google_light_normal_ios.svg'
 
 function SignUpScreen({ navigation }) {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const SignUp = e => {
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        alert('User account created & signed in!');
+      })
+      .catch(error => {
+        if (error.code === 'auth/email-already-in-use') {
+          alert('That email address is already in use!');
+        }
+
+        if (error.code === 'auth/invalid-email') {
+          alert('That email address is invalid!');
+        }
+
+        console.error(error);
+      });
+  }
+
   const SignUpWithGoogle = e => { };
 
   return (
@@ -16,14 +40,20 @@ function SignUpScreen({ navigation }) {
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Sign up</Text>
               <TextInput
+                value={email}
+                onChangeText={setEmail}
+                keyboardType="email-address"
                 style={styles.textInput}
                 placeholder="Enter your email" />
               <TextInput
+                value={password}
+                onChangeText={setPassword}
                 style={styles.textInput}
+                secureTextEntry={true}
                 placeholder="Enter your password" />
               <Button
-                title="Log in"
-                onPress={() => navigation.navigate('Log in')} />
+                title="Sign up"
+                onPress={SignUp} />
             </View>
 
             <View style={{ ...styles.sectionContainer, flex: 1, alignItems: 'center' }}>
