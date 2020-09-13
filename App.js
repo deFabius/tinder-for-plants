06adea5b7d4b453cf0b7bdc3e7974f4d98e63b4d
Fabio Valle <fabio.valle@gmail.com>
2020-09-13 16:36:52 +0100

    Reorganize main navigation to allow modals indipendently from tab

--- App.js
+++ App.js
@@ -1,63 +1,34 @@
-import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import HomeScreen from './components/screens/HomeScreen';
-import SearchScreen from './components/screens/SearchScreen';
-import MyPropagationScreen from './components/screens/MyPropagationsScreen';
-import WishListScreen from './components/screens/WishListScreen';
-import MyAccountScreen from './components/screens/MyAccountScreen';
-import HomeIcon from './components/shared/icons/home-outline.svg';
-import SearchIcon from './components/shared/icons/search-outline.svg';
-import MyPropagationIcon from './components/shared/icons/leaf-outline.svg';
-import WishListIcon from './components/shared/icons/heart-outline.svg';
-import MyAccountIcon from './components/shared/icons/person-outline.svg';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const Tab = createBottomTabNavigator();
+import reducers from './src/reducers';
 
-const App: () => React$Node = () => {
+import MainNavigator from './src/components/navigators/MainNavigator';
+import LoginNavigator from './src/components/navigators/LoginNavigator';
+
+const RootStack = createStackNavigator();
+const store = createStore(reducers);
+
+function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({route}) => ({
-          tabBarIcon: ({focused, color, size}) => {
-            let Icon;
-
-            const iconStyle = {
-              height: size,
-              stroke: color,
-              color: color,
-            };
-
-            switch (route.name) {
-              case 'Home':
-                Icon = <HomeIcon {...iconStyle} />;
-                break;
-              case 'Search':
-                Icon = <SearchIcon {...iconStyle} />;
-                break;
-              case 'My Propagations':
-                Icon = <MyPropagationIcon {...iconStyle} />;
-                break;
-              case 'Wish List':
-                Icon = <WishListIcon {...iconStyle} />;
-                break;
-              case 'My Account':
-                Icon = <MyAccountIcon {...iconStyle} />;
-                break;
-            }
-
-            // You can return any component that you like here!
-            return Icon;
-          },
-        })}>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Search" component={SearchScreen} />
-        <Tab.Screen name="My Propagations" component={MyPropagationScreen} />
-        <Tab.Screen name="Wish List" component={WishListScreen} />
-        <Tab.Screen name="My Account" component={MyAccountScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <RootStack.Navigator mode="modal">
+          <RootStack.Screen
+            name="Main"
+            component={MainNavigator}
+            options={{ headerShown: false }}
+          />
+          <RootStack.Screen name="LogIn"
+            component={LoginNavigator}
+            options={{ headerShown: false }} />
+        </RootStack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
-};
+}
 
 export default App;
